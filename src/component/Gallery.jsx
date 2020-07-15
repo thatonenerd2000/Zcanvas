1599102e6fea04e1e2966215b321339245933853
Shadman <chowdhuryshadman0@gmail.com>
2020-07-14 22:38:47 -0400

    Some animation and fix

--- src/component/Gallery.jsx
+++ src/component/Gallery.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import {Palette} from 'react-palette'
 import "../css/index.scss"
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Fade from 'react-reveal/Fade';
 
 import temp1 from "../media/temp_drawings/temp1.jpg"
 import temp2 from "../media/temp_drawings/temp2.jpg"
@@ -15,54 +16,78 @@ const Gallery = ()=>{
         <>
             <Palette src={temp1}>
             {({data}) => (
-                <div className="Gallery_palette" style={{backgroundColor:data.vibrant}}>
-                    <img className="Gallery_canvas" src={temp1} alt="canvas_image"></img>
-                </div>
+                <Fade>
+                    <div className="Gallery_palette" style={{backgroundColor:data.vibrant}}>
+                        <Fade delay={500}>
+                            <img className="Gallery_canvas" src={temp1} alt="canvas_image"></img>
+                        </Fade>
+                    </div>
+                </Fade>
             )}
             </Palette>
 
 
             <Palette src={temp2}>
             {({data}) => (
-                <div className="Gallery_palette" style={{backgroundColor:data.vibrant}}>
-                    <img className="Gallery_canvas" src={temp2} alt="canvas_image"></img>
-                </div>
+                <Fade>
+                    <div className="Gallery_palette" style={{backgroundColor:data.vibrant}}>
+                        <Fade delay={500}>
+                            <img className="Gallery_canvas" src={temp2} alt="canvas_image"></img>
+                        </Fade>
+                    </div>
+                </Fade>
             )}
             </Palette>
 
 
             <Palette src={temp3}>
             {({data}) => (
-                <div className="Gallery_palette" style={{backgroundColor:data.vibrant}}>
-                    <img className="Gallery_canvas" src={temp3} alt="canvas_image"></img>
-                </div>
+                <Fade>
+                    <div className="Gallery_palette" style={{backgroundColor:data.vibrant}}>
+                        <Fade delay={500}>
+                            <img className="Gallery_canvas" src={temp3} alt="canvas_image"></img>
+                        </Fade>
+                    </div>
+                </Fade>
             )}
             </Palette>
 
 
             <Palette src={temp4}>
             {({data}) => (
-                <div className="Gallery_palette" style={{backgroundColor:data.vibrant}}>
-                    <img className="Gallery_canvas" src={temp4} alt="canvas_image"></img>
-                </div>
+                <Fade>
+                    <div className="Gallery_palette" style={{backgroundColor:data.vibrant}}>
+                        <Fade delay={500}>
+                            <img className="Gallery_canvas" src={temp4} alt="canvas_image"></img>
+                        </Fade>
+                    </div>
+                </Fade>
             )}
             </Palette>
 
 
             <Palette src={temp5}>
             {({data}) => (
-                <div className="Gallery_palette" style={{backgroundColor:data.vibrant}}>
-                    <img className="Gallery_canvas" src={temp5} alt="canvas_image"></img>
-                </div>
+                <Fade>
+                    <div className="Gallery_palette" style={{backgroundColor:data.vibrant}}>
+                        <Fade delay={500}>
+                            <img className="Gallery_canvas" src={temp5} alt="canvas_image"></img>
+                        </Fade>
+                    </div>
+                </Fade>
             )}
             </Palette>
 
 
             <Palette src={temp6}>
             {({data}) => (
-                <div className="Gallery_palette" style={{backgroundColor:data.vibrant}}>
-                    <img className="Gallery_canvas" src={temp6} alt="canvas_image"></img>
-                </div>
+                <Fade>
+                    <div className="Gallery_palette" style={{backgroundColor:data.vibrant}}>
+                        <Fade delay={500}>
+                            <img className="Gallery_canvas" src={temp6} alt="canvas_image"></img>
+                        </Fade>
+                    </div>
+                </Fade>
             )}
             </Palette>
             
